23c9af89d8c3f3ed258b56ce91a0a7d372c3d5b2
Shahriyor - Keypix <78787657+shahriyor-sharifjonov@users.noreply.github.com>
2022-02-05 16:25:01 +0500

    initial

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 function pageTransition() {
     var tl = gsap.timeline();
     tl.to('ul.transition li', {duration: .5, scaleY: 1, transformOrigin: "bottom left", stagger: .2})
-    tl.to('ul.transition li', {duration: .5, scaleY: 0, transformOrigin: "bottom left", stagger: .1, delay: .1})    
+    tl.to('ul.transition li', {duration: .5, scaleY: 0, transformOrigin: "bottom left", stagger: .1, delay: .1})
 }
 
 function contentAnimation(){
